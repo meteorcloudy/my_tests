a47191f508739c9f8bcdd52f2d669b13b5b08bc7
Yun Peng <pcloudy@google.com>
2018-08-21 15:13:58 +0200

    Add symlink_test

--- run_under_test/my_setup.sh
+++ run_under_test/my_setup.sh
@@ -1,2 +1,21 @@
 export MY_ENV="hello"
-$@
+
+RUNFILES_MANIFEST_FILE="${TEST_SRCDIR}/MANIFEST"
+
+function rlocation() {
+  if is_absolute "$1" ; then
+    # If the file path is already fully specified, simply return it.
+    echo "$1"
+  elif [[ -e "$TEST_SRCDIR/$1" ]]; then
+    # If the file exists in the $TEST_SRCDIR then just use it.
+    echo "$TEST_SRCDIR/$1"
+  elif [[ -e "$RUNFILES_MANIFEST_FILE" ]]; then
+    # If a runfiles manifest file exists then use it.
+    echo "$(grep "^$1 " "$RUNFILES_MANIFEST_FILE" | sed 's/[^ ]* //')"
+  fi
+}
+EXE="${1#./}"
+TEST_BINARY="$(rlocation $TEST_WORKSPACE/$EXE)"
+shift
+echo test=$TEST_BINARY
+$TEST_BINARY $@
---
+++ symlink_test/main.cc
@@ -0,0 +1,99 @@
+#include <Windows.h>
+#include <iostream>
+#include <string.h>
+#include <ctime>
+#include <string>
+
+#ifndef SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE
+#define SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE 0x2
+#endif
+
+#ifndef SYMBOLIC_LINK_FLAG_DIRECTORY
+#define SYMBOLIC_LINK_FLAG_DIRECTORY 0x2
+#endif
+
+using namespace std;
+
+void CreateSymlinksOnFiles(int count) {
+    const wchar_t* target = L"target_file";
+    const wchar_t* linkDir = L"links_for_file/";
+
+    CreateDirectoryW(linkDir, NULL);
+
+    // Create
+    clock_t begin = clock();
+
+    for (int i = 0; i < count; i++) {
+        wstring link = wstring(linkDir) + to_wstring(i);
+        if (CreateSymbolicLinkW(link.c_str(), L"target_file", SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE) == 0) {
+            wcout << "Error: Symlink creation failed!" << endl;
+        }
+    }
+
+    clock_t end = clock();
+
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+
+    printf("Time for creating %d file symlinks: %lf\n", count, elapsed_secs);
+
+    // Delete
+    begin = clock();
+
+    for (int i = 0; i < count; i++) {
+        wstring link = wstring(linkDir) + to_wstring(i);
+        if (DeleteFileW(link.c_str()) == 0) {
+            wcout << "Error: Symlink deleting failed!" << endl;
+        }
+    }
+
+    end = clock();
+
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    printf("Time for deleting %d file symlinks: %lf\n", count, elapsed_secs);
+}
+
+void CreateSymlinksOnDirectories(int count) {
+
+    const wchar_t* target = L"target_dir";
+    const wchar_t* linkDir = L"links_for_dir/";
+
+    CreateDirectoryW(linkDir, NULL);
+
+    // Create
+    clock_t begin = clock();
+
+    for (int i = 0; i < count; i++) {
+        wstring link = wstring(linkDir) + to_wstring(i);
+        if (CreateSymbolicLinkW(link.c_str(), target, SYMBOLIC_LINK_FLAG_DIRECTORY | SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE) == 0) {
+            wcout << "Error: Symlink creation failed!" << endl;
+        }
+    }
+
+    clock_t end = clock();
+
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+
+    printf("Time for creating %d directory symlinks: %lf\n", count, elapsed_secs);
+
+
+    // Delete
+    begin = clock();
+
+    for (int i = 0; i < count; i++) {
+        wstring link = wstring(linkDir) + to_wstring(i);
+        if (RemoveDirectoryW(link.c_str()) == 0) {
+            wcout << "Error: Symlink deleting failed!" << endl;
+        }
+    }
+
+    end = clock();
+
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    printf("Time for deleting %d directory symlinks: %lf\n", count, elapsed_secs);
+}
+
+int main() {
+    CreateSymlinksOnFiles(10000);
+    CreateSymlinksOnDirectories(10000);
+    return 0;
+}
