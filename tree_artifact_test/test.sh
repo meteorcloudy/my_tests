d2befaaf1439610ae95509abe60d3dbfae7e6a4a
Yun Peng <pcloudy@google.com>
2019-02-21 10:45:32 +0100

    Add test.sh

---
+++ tree_artifact_test/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+gsutil cp gs://bazel-builds/artifacts/ubuntu1404/$1/bazel ~/bin/bazel-$1 && chmod +x ~/bin/bazel-$1
+alias bazel=/home/bazel/bin/bazel-$1
+
+for i in `seq 1 5`; do
+  bazel clean --expunge && bazel build --show_progress_rate_limit=5 --curses=yes --color=yes --verbose_failures --keep_going --jobs=32 --announce_rc --experimental_multi_threaded_digest --sandbox_tmpfs_path=/tmp --remote_timeout=2 --disk_cache= --remote_max_connections=2000 --host_platform_remote_properties_override='properties:{name:"platform" value:"ubuntu1604"}' --google_default_credentials --remote_http_cache=https://storage.googleapis.com/pcloudy-test //...
+done
