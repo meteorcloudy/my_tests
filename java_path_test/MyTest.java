328a45d155ac025ce6a6cef4e0b65145f25130a1
Yun Peng <pcloudy@google.com>
2019-03-12 12:29:18 +0100

    Add comment

--- java_path_test/MyTest.java
+++ java_path_test/MyTest.java
@@ -2,6 +2,8 @@ import java.nio.file.Paths;
 
 public class MyTest {
   public static void main(String[] args) {
+    // On Windows, the result is ..\foo\bar
+    // On Linux, the result is foo/bar
     System.out.println(Paths.get("").relativize(Paths.get("foo/bar")));
   }
 }
